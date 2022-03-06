3922f5cca78dbe2ff6d566e131f68bd5384b1360
mostafashalaby24 <99189624+mostafashalaby24@users.noreply.github.com>
2022-03-06 14:12:20 +0200

    Kafka 2

---
+++ Kafka2/Kafka2/src/main/java/com/example/Kafka2/Config.java
@@ -0,0 +1,35 @@
+package com.example.Kafka2;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.awt.print.Book;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class Config {
+    @Bean
+    public ProducerFactory<String, Book> producerFactory()
+    {
+        Map<String,Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return  new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate kafkaTemplate()
+    {
+        return  new KafkaTemplate<>(producerFactory());
+    }
+}
---
+++ Kafka2/Kafka2/src/main/java/com/example/Kafka2/controller.java
@@ -0,0 +1,22 @@
+package com.example.Kafka2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class controller {
+
+    @Autowired
+    KafkaTemplate<String,Book> kafkaTemplate;
+
+    private static final String TOPIC = "NewTopic";
+
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody Book book)
+    {
+        kafkaTemplate.send(TOPIC, book);
+        return "Published Successfully!";
+    }
+
+}
